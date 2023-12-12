d2edab87fc47d3a3d108ffba05d6b25cd2cf5f4c
Jim Jones <jim.jones@uni-muenster.de>
2023-12-12 17:58:38 +0100

    add new regression tests

--- sql/graphdb-getty.sql
+++ sql/graphdb-getty.sql
@@ -45,3 +45,49 @@ WHERE name = 'West Flanders, Flanders, Belgium, Europe, World';
 SELECT DISTINCT ON (name) name, lon, lat
 FROM getty_places
 WHERE lat BETWEEN 52.5 AND 53.0;
+
+/*
+ * == Non-Italians Who Worked in Italy ==
+ * Find non-Italians who worked in Italy and lived during a given time range
+ *  Having event that took place in tgn:1000080 Italy or any of its descendants
+ *  Birth date between 1250 and 1780
+ *  Just for variety, we look for artists as descendants of facets ulan:500000003 "Corporate bodies" or ulan:500000002 "Persons, Artists", rather than having type "artist" as we did in previous queries. In the previous query we used values{..} but we here use filter(in(..)).
+ *  Not having nationality aat:300111198 Italian or any of its descendants
+ */
+
+CREATE FOREIGN TABLE getty_non_italians (
+  uri text   OPTIONS (variable '?x'),
+  name text  OPTIONS (variable '?name'),
+  bio text   OPTIONS (variable '?bio'),
+  birth int  OPTIONS (variable '?birth')
+)
+SERVER getty OPTIONS (
+  log_sparql 'true',
+  sparql '
+  PREFIX ontogeo: <http://www.ontotext.com/owlim/geo#>
+  PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+  PREFIX gvp: <http://vocab.getty.edu/ontology#>
+  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  PREFIX schema: <http://schema.org/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+   SELECT ?x ?name ?bio ?birth {
+  {SELECT DISTINCT ?x
+    {?x foaf:focus/bio:event/(schema:location|(schema:location/gvp:broaderExtended)) tgn:1000080-place}}
+ 	 ?x gvp:prefLabelGVP/xl:literalForm ?name;
+	    foaf:focus/gvp:biographyPreferred [
+	    schema:description ?bio;
+       	gvp:estStart ?birth].
+
+  FILTER ("1250"^^xsd:gYear <= ?birth && ?birth <= "1780"^^xsd:gYear)
+  FILTER EXISTS {?x gvp:broaderExtended ?facet.
+  FILTER(?facet in (ulan:500000003, ulan:500000002))}
+  FILTER NOT EXISTS {?x foaf:focus/(schema:nationality|(schema:nationality/gvp:broaderExtended)) aat:300111198}}
+  '); 
+
+
+SELECT name, bio, birth
+FROM getty_non_italians
+WHERE bio ~~* '%artist%'
+ORDER BY birth 
+LIMIT 10;
