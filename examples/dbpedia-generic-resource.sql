2f0e211f023ef8ab6fd1fd03ae45906605ffbff4
Jim Jones <jim.jones@uni-muenster.de>
2023-12-25 20:45:33 +0100

    Add comments and FILTER expressions to SPARQL query

--- examples/dbpedia-generic-resource.sql
+++ examples/dbpedia-generic-resource.sql
@@ -3,11 +3,16 @@ CREATE SERVER dbpedia
 FOREIGN DATA WRAPPER rdf_fdw 
 OPTIONS (endpoint 'https://dbpedia.org/sparql');
 
+/*
+ * All non IRI property values directly linked to a given resource.
+ * Only numeric literals or literals written in English are accepted.
+ * Author: Jim Jones
+ */
 CREATE FOREIGN TABLE dbpedia_resource (
-  uri text        OPTIONS (variable '?uri'),
-  property_uri text       OPTIONS (variable '?propuri'),
-  property text  OPTIONS (variable '?proplabel'),
-  value text      OPTIONS (variable '?val')
+  uri text           OPTIONS (variable '?uri'),
+  property_uri text  OPTIONS (variable '?propuri'),
+  property text      OPTIONS (variable '?proplabel'),
+  value text         OPTIONS (variable '?val')
 )
 SERVER dbpedia OPTIONS (
   log_sparql 'true',
@@ -16,19 +21,25 @@ SERVER dbpedia OPTIONS (
   SELECT ?prop ?val
   WHERE {
     ?uri ?propuri ?val .
-    ?propuri rdfs:label ?proplabel
+    ?propuri rdfs:label ?proplabel .
+    FILTER(!isIRI(?val))
+    FILTER(LANG(?val) = "en" || LANG(?val) = "")
+    FILTER(LANG(?proplabel) = "en")
   } 
 ');
-
 
 /*
  * Sir Edward Elgar (british composer)
  * Wikipedia Page   : https://en.wikipedia.org/wiki/Edward_Elgar
  * DBpedia resource : http://dbpedia.org/resource/Edward_Elgar 
+ *
+ * List only the subject's abstract
  */ 
 SELECT property, value 
 FROM dbpedia_resource
-WHERE uri = 'http://dbpedia.org/resource/Edward_Elgar'
+WHERE 
+  uri = 'http://dbpedia.org/resource/Edward_Elgar' AND 
+  property = 'has abstract'
 ORDER BY property;
 
 
@@ -36,8 +47,10 @@ ORDER BY property;
  * Isle of Man
  * Wikipedia Page   : https://en.wikipedia.org/wiki/Isle_of_Man
  * DBpedia resource : http://dbpedia.org/resource/Isle_of_Man
+ * 
+ * List all non IRI properties directly related to the subject
  */ 
-SELECT property, value 
+SELECT property, value
 FROM dbpedia_resource
 WHERE uri = 'http://dbpedia.org/resource/Isle_of_Man'
 ORDER BY property;
