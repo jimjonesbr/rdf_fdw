c7af885879f23cb2f4cb851e06d9b8cb510e7f27
Jim Jones <jim.jones@uni-muenster.de>
2024-03-12 19:46:36 +0100

    add geom column to foreign table

--- examples/dbpedia-cities-table-cloning.sql
+++ examples/dbpedia-cities-table-cloning.sql
@@ -4,21 +4,23 @@ OPTIONS (endpoint 'https://dbpedia.org/sparql');
 
 CREATE FOREIGN TABLE public.cities (
   uri text       OPTIONS (variable '?city', nodetype 'iri'),
+  geom text      OPTIONS (variable '?wkt', nodetype 'literal', literaltype 'xsd:string'),
   city_name text OPTIONS (variable '?name', nodetype 'literal', literaltype 'xsd:string'),
   area numeric   OPTIONS (variable '?area', nodetype 'literal', literaltype 'xsd:double')
 )
 SERVER dbpedia OPTIONS (
-  log_sparql 'false',
   sparql '
     PREFIX dbo:  <http://dbpedia.org/ontology/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
     SELECT *
     {
       {
-        SELECT ?city ?name ?area
+        SELECT ?city ?name ?area ?wkt
         {?city a dbo:City ;
         foaf:name ?name .
         OPTIONAL {?city dbo:areaTotal ?area}
+        OPTIONAL {?city geo:geometry ?wkt}
       } ORDER BY ASC(?name)
     }
   }
