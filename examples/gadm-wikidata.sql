40ba339cc173a2310751e1b695622938fce47138
Jim Jones <jim.jones@uni-muenster.de>
2024-05-04 22:33:28 +0200

    Update gadm<->wikidata mapping script

--- examples/gadm-wikidata.sql
+++ examples/gadm-wikidata.sql
@@ -1,20 +1,26 @@
 /*
- ogr2ogr -f PostgreSQL "PG:dbname=gadm host=172.17.0.2 user=postgres" gadm41_DEU.gpkg
-*/
-
-CREATE SERVER wikidata
-FOREIGN DATA WRAPPER rdf_fdw 
-OPTIONS (endpoint 'https://query.wikidata.org/sparql');
-
+ * This example shows how to harvest Wikidata to retrieve the identidiers of
+ * German cities. It assumes the GADM data for Germany is downloaded and imported
+ * into PostgreSQL - with PostGIS the extension.
+ *
+ * GADM data for Germany: https://geodata.ucdavis.edu/gadm/gadm4.1/gpkg/gadm41_DEU.gpkg
+ * 
+ * The GADM GeoPackage can be imported into a PostgreSQL using ogr2ogr as follows:
+ *
+ * $ ogr2ogr -f PostgreSQL "PG:dbname=db host=pgserver user=postgres" gadm41_DEU.gpkg
+ *
+ * To map the GADM data to Wikidata, GeoNames or DNB identifiers we first create two tables
+ * to store the source and the mapping itself:
+ */
 CREATE TABLE external_source (
     source_id text,
-    source_url text,
+    base_url text,
     PRIMARY KEY (source_id)
 );
 
 INSERT INTO external_source 
 VALUES 
-  ('wikidata', 'https://query.wikidata.org'),
+  ('wikidata', 'http://www.wikidata.org/entity/'),
   ('gnd',      'https://www.dnb.de'),
   ('geonames', 'https://www.geonames.org/');
 
@@ -25,12 +31,20 @@ CREATE TABLE gadm_mapping (
   PRIMARY KEY (gadm_id, source_id)
 );
 
+/*
+ * Now we create a FOREIGN TABLE with a SPARQL query that searches for
+ * German cities and municipalites.
+ */
+CREATE SERVER wikidata
+FOREIGN DATA WRAPPER rdf_fdw 
+OPTIONS (endpoint 'https://query.wikidata.org/sparql');
+
 CREATE FOREIGN TABLE wikidata_german_cities (
-  wikidataid text            OPTIONS (variable '?wikidataid'),
-  geonamesid text            OPTIONS (variable '?geonamesid'),
-  gndid text                 OPTIONS (variable '?gndid'),
-  name text                  OPTIONS (variable '?label', language 'de'),
-  geom geometry(point, 4326) OPTIONS (variable '?geo')
+  wikidataid text            OPTIONS (variable '?wikidataid', nodetype 'iri'),
+  geonamesid text            OPTIONS (variable '?geonamesid', nodetype 'iri'),
+  gndid text                 OPTIONS (variable '?gndid', nodetype 'iri'),
+  name text                  OPTIONS (variable '?label', nodetype 'literal', language 'de'),
+  geom geometry(point, 4326) OPTIONS (variable '?geo', nodetype 'literal')
 )
 SERVER wikidata OPTIONS (
   log_sparql 'false',
@@ -50,6 +64,10 @@ SERVER wikidata OPTIONS (
 ');
 
 
+/*
+ * Finally we iterate over all records of the GADM data set and look for matches in 
+ * Wikipedia using the just created FOREIGN TABLE.
+ */
 DO $$
 DECLARE 
   g record; 
@@ -59,27 +77,30 @@ BEGIN
   -- Iterate over all cities / municipalities from Germany
   FOR g IN 
     SELECT DISTINCT gid_4, name_4, type_4, geom
-    FROM adm_adm_4 
-    WHERE gid_0 = 'DEU' 
+    FROM adm_adm_4 j
+    WHERE gid_0 = 'DEU' AND NOT EXISTS (SELECT 1 FROM gadm_mapping WHERE gadm_id = j.gid_4)
     ORDER BY name_4
   LOOP
     -- Look in Wikidata for cities that match the GADM city name
     FOR w IN
-      EXECUTE FORMAT('SELECT * FROM wikidata_german_cities WHERE name = %s',quote_literal(g.name_4))
+      EXECUTE format('SELECT * FROM wikidata_german_cities WHERE name = %s', quote_literal(g.name_4))
     LOOP
       -- Is the retrieved geometry at least 5km away from the GADM geometry?
       IF ST_Distance(w.geom::geography, g.geom::geography) < 5000 THEN 
-	      INSERT INTO gadm_mapping VALUES (g.gid_4, 'wikidata', w.wikidataid);
+	      INSERT INTO gadm_mapping VALUES (g.gid_4, 'wikidata', replace(w.wikidataid,'http://www.wikidata.org/entity/',''));
 	      RAISE INFO '[OK] GADM "% (%)" mapped to Wikidata "% (%)"',g.name_4, g.gid_4, w.name, w.wikidataid;
         match := true;
 
+        -- Store the GND identifier, if the match has any.
         IF w.gndid IS NOT NULL THEN
           INSERT INTO gadm_mapping VALUES (g.gid_4, 'gnd', w.gndid);
         END IF;
 
+        -- Store the GeoNames identifier, if the match has any.
         IF w.geonamesid IS NOT NULL THEN
           INSERT INTO gadm_mapping VALUES (g.gid_4, 'geonames', w.geonamesid);
         END IF;
+
         -- Leaving loop, as at this point we've already found a match.
         EXIT;
       END IF;      
@@ -87,15 +108,10 @@ BEGIN
     END LOOP;
 
     IF NOT match THEN
-      RAISE WARNING 'No match found for % (%)',g.name_4, g.gid_4;
+      RAISE WARNING 'No match found for % (%).',g.name_4, g.gid_4;
     END IF;
 
+    COMMIT;
   END LOOP;
 END;
 $$;
-
-
-
--- DROP FOREIGN TABLE IF EXISTS german_cities; 
--- SELECT * FROM german_cities limit 11;
--- DROP TABLE IF EXISTS external_source CASCADE;
